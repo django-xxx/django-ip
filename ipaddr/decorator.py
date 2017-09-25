4997d7cb1a6e6022467cb059dce15a020f7e3310
Brightcells <brightcells@gmail.com>
2017-09-25 12:30:19 +0800

    :sparkles: Add decorator check_ip

---
+++ ipaddr/decorator.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from functools import wraps
+
+from django.http import HttpResponseForbidden
+from ipaddr.clientip import client_ip
+
+
+def check_ip(func=None, ip=None):
+    def decorator(func):
+        @wraps(func)
+        def legal_ip(request, *args, **kwargs):
+            if ip and ip != client_ip(request):
+                return HttpResponseForbidden()
+            return func(request, *args, **kwargs)
+        return legal_ip
+
+    if not func:
+        def decorator2(func):
+            return decorator(func)
+        return decorator2
+
+    return decorator(func)
